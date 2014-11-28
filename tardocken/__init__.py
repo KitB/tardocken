9b306c44454b3bae0f6d8bcf1138db219a7bccd2
Kit Barnes <k.barnes@mhnltd.co.uk>
2014-11-28 17:10:46 +0000

    Obey the license

--- tardocken/__init__.py
+++ tardocken/__init__.py
@@ -1,5 +1,6 @@
 """
    Copyright 2014 n@work Internet Informationssysteme GmbH, Germany
+   Copyright 2014 Mobile Healthcare Networks Ltd., UK
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -12,6 +13,8 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+
+   Originally by https://github.com/itsafire, modifications by https://github.com/KitB.
 """
 
 import argparse
